a030ff879c30c26462c4cd0f512dd54c5d04680f
Chima Njoku <chimae.njoku@gmail.com>
2025-06-17 11:44:51 +0100

    Exercise 14h

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -29,7 +29,7 @@ cart.forEach((cartItem) => {
 
   cartSummaryHTML += `
   <div class="cart-item-container 
-  js-cart-item-container-${matchingProduct.id}">
+  js-cart-item-container-${matchingProduct.id} ">
             <div class="delivery-date">Delivery date: Tuesday, June 21</div>
 
             <div class="cart-item-details-grid">
@@ -127,6 +127,10 @@ document.querySelectorAll(".js-update-link").forEach((link) => {
   link.addEventListener("click", () => {
     const productId = link.dataset.productId;
 
-    console.log(productId);
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+
+    container.classList.add("is-editing-quantity");
   });
 });
