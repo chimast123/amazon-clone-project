0c04de1199a92b566924969a43baf219de8294fa
Chima Njoku <chimae.njoku@gmail.com>
2025-06-25 11:19:32 +0100

    June 25th

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -8,6 +8,7 @@ import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 import { hello } from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
 import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
+import { deliveryOptions } from "../data/deliveryOptions.js";
 
 hello();
 
@@ -124,7 +125,9 @@ cart.forEach((cartItem) => {
           </div>`;
 });
 
-function deliveryOptionsHTML() {}
+function deliveryOptionsHTML() {
+  deliveryOptions.forEach(() =>)
+}
 
 document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
 
